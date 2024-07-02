a0bd0ab0736aec34f1eb4e5ff3815d7880ff53c4
mxinseo <jade201913@gmail.com>
2024-07-03 01:35:24 +0900

    Feat : FreeboardPostModel ViewSet 구현

--- Backend/freeboard/views.py
+++ Backend/freeboard/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+
+from .models import FreeboardPost
+from .serializers import FreeboardPostModelSerializer
 
 # Create your views here.
+class FreeboardPostModelViewSet(ModelViewSet):
+    queryset=FreeboardPost.objects.all()
+    serializer_class=FreeboardPostModelSerializer
