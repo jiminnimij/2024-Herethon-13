812c39780c27ab09cacf6c2f9ae2e2dc6c0870b2
mxinseo <jade201913@gmail.com>
2024-07-03 20:24:58 +0900

    Feat : url 연결

--- Backend/freeboard/urls.py
+++ Backend/freeboard/urls.py
@@ -1,13 +1,18 @@
 from django.urls import path, include
-from .views import FreeboardPostModelViewSet
+from .views import FreeboardPostModelViewSet, FreeboardPostCommentModelViewSet
 from rest_framework import routers
 
-# URL = freeboard/ 로 시작
-app_name = 'freeboard'
 
-# ViewSet Router
+# URL = freeboard/ 로 시작, 기본 라우터 설정
+app_name = 'freeboard'
 router = routers.DefaultRouter()
-router.register('', FreeboardPostModelViewSet)
+
+# 자유게시판 ViewSet Router
+router.register(r'post', FreeboardPostModelViewSet)
+
+# 자유게시판 댓글 Viewset url 연결
+router.register(r'comment', FreeboardPostCommentModelViewSet)
+
 
 urlpatterns = [
     path('', include(router.urls)),
