abc74658f383224ce859e2aa7c9105902e782144
mxinseo <jade201913@gmail.com>
2024-07-03 20:24:01 +0900

    Feat : admin site에 모델 등록

--- Backend/freeboard/admin.py
+++ Backend/freeboard/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import FreeboardPost
+from .models import FreeboardPost, FreeboardPostComment
 
 # Register your models here.
 admin.site.register(FreeboardPost)
+admin.site.register(FreeboardPostComment)
