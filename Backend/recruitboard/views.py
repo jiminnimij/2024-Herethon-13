07006aa0f15d68068e4391577c9053aa1c01d195
mxinseo <jade201913@gmail.com>
2024-07-03 21:14:58 +0900

    Feat: RecruitboardPost ViewSet 구현

--- Backend/recruitboard/views.py
+++ Backend/recruitboard/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+
+from .models import RecruitboardPost
+from .serializers import RecruitboardPostModelSerializer
 
 # Create your views here.
+
+# 채용게시판 게시글 뷰셋
+class RecruitboardPostModelViewSet(ModelViewSet):
+    queryset=RecruitboardPost.objects.all()
+    serializer_class=RecruitboardPostModelSerializer
