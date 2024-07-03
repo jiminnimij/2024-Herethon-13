b8ae3c6228646384110ddb068434aeb928cc8591
mxinseo <jade201913@gmail.com>
2024-07-03 21:04:16 +0900

    Feat: settings.py에 앱 추가

--- Backend/shesplace/settings.py
+++ Backend/shesplace/settings.py
@@ -37,8 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # DRF
     'rest_framework',
-    'freeboard'
+
+    # internal APP
+    'freeboard',
+    'recruitboard',
 ]
 
 MIDDLEWARE = [
