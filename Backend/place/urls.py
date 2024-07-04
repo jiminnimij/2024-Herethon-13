6ed0f23912272ef509935d1d1ca1cb5d36fb60c4
mxinseo <jade201913@gmail.com>
2024-07-04 23:52:57 +0900

    Feat: url 연결

--- Backend/place/urls.py
+++ Backend/place/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import SimpleRouter
-from .views import ReviewViewSet, PlaceViewSet
+from .views import ReviewViewSet, PlaceViewSet, ScrapViewSet
 
 # 기본 라우터 설정
 app_name = 'place'
@@ -11,6 +11,9 @@ router.register(r'place', PlaceViewSet, basename='place')
 
 # 리뷰 Viewset url 연결
 router.register(r'review', ReviewViewSet, basename='review')
+
+# 스크랩 Viewset url 연결
+router.register(r'scrap', ScrapViewSet, basename='scrap')
 
 urlpatterns = [
     path('', include(router.urls)),
